755188324b538ef8aad007e059c7141d08243df4
wijdan-alayidh <wijdan_alayidh@hotmail.com>
2021-06-12 03:07:40 +0300

    Update the css and html for buttons and adding the main script for project

--- day 2-Progress  Steps/script.js
+++ day 2-Progress  Steps/script.js
@@ -1,1 +1,43 @@
 "use strict";
+
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+const maxWidth = circles.length;
+
+// click on next button
+next.addEventListener("click", () => {
+  currentActive >= maxWidth ? currentActive == maxWidth : currentActive++;
+
+  update();
+});
+
+// click on next button
+prev.addEventListener("click", () => {
+  currentActive <= 1 ? currentActive == 1 : currentActive--;
+
+  update();
+});
+
+// update function to update the active buttons and progress bar width
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+
+    let actives = document.querySelectorAll(".active");
+    progress.style.width = ((actives.length - 1) / (maxWidth - 1)) * 100 + "%";
+
+    currentActive === 1 ? (prev.disabled = true) : (prev.disabled = false);
+
+    currentActive === maxWidth
+      ? (next.disabled = true)
+      : (next.disabled = false);
+  });
+}
